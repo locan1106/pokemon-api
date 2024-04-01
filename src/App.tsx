bffa4d32c4efe13b4e18c1ac27baf1f708cfb34d
locan1106 <phamlocan1106@gmail.com>
2024-04-01 10:26:01 +0700

    create header and css header, body

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import "./App.css";
 
-function App() {
-	return <div className="App"></div>;
-}
+const App: React.FC = () => {
+	return (
+		<div className="App">
+			<div className="container">
+				<header className="pokemon-header">Pokemon</header>
+			</div>
+		</div>
+	);
+};
 
 export default App;
